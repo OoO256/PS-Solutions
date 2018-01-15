50fe21cd15f15ca34858b8e60efe64134c29ab33
Yonguk Lee <qjrmsktso2@gmail.com>
2018-01-15 20:10:46 +0900

    Update Fare Dodging.cpp
    Use int data structure so that reduce running time and space.

--- Fare Dodging.cpp
+++ Fare Dodging.cpp
@@ -1,15 +1,3 @@
-/*
-this code is a solution of Fare Dodging
-from ACM-ICPC > Regionals > Europe > Northwestern European Regional Contest > Benelux Algorithm Programming Contest > BAPC 2013 Preliminaries #F
-https://www.acmicpc.net/problem/9323
-
-programer: 이용욱 Yonguk Lee 
-email: qjrmsktso2@gmail.com
-
-I hope you can learn sth from mine!
-*/
-
-
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -22,9 +10,9 @@ class road
 public:
 	int dist;
 	int dest;
-	float p;
+	int p;
 
-	road(int a, int b, float c) {
+	road(int a, int b, int c) {
 		dist = a;
 		dest = b;
 		p = c;
@@ -33,77 +21,70 @@ public:
 class path
 {
 public:
-	float min;
-	float dist_0; //무임
-	float dist_1; //유임
+	int min;
+	int dist_0; //무임
+	int dist_1; //유임
 	int dest;
 
-	path(float a, float b, float c, int d) {
-		min=a;
-		dist_0=b;
-		dist_1=c;
-		dest=d;
+	path(int a, int b, int c, int d) {
+		min = a;
+		dist_0 = b;
+		dist_1 = c;
+		dest = d;
 	};
 };
-struct compair_path {
-	bool operator()(path a, path b) {
-		return a.min > b.min;
-	}
-};
+bool operator>(path a, path b) {
+	return a.min > b.min;
+}
 
 int n_city, n_road, start, endcity, basicprice, price_1, basicfine;
 vector<road>edge[205];
 
-float min(float a, float b) {
-	if (a < b) return a;
-	else return b;
-}
 
 void getinput() {
 	scanf("%d %d %d %d %d %d %d", &n_city, &n_road, &start, &endcity, &basicprice, &price_1, &basicfine);
 	int i_start, i_end, i_dist;
-	float  i_p;
+	int  i_p;
 	for (int i = 0; i < n_road; i++) {
-		scanf("%d %d %f %d", &i_start, &i_end, &i_p, &i_dist);
+		scanf("%d %d %d %d", &i_start, &i_end, &i_p, &i_dist);
 
-		edge[i_start].push_back(road(i_dist, i_end, i_p/100));
-		edge[i_end].push_back(road(i_dist, i_start, i_p / 100));
+		edge[i_start].emplace_back(i_dist, i_end, i_p);
+		edge[i_end].emplace_back(i_dist, i_start, i_p);
 	}
 }
 
-float dijkstra() {
-	float dist[205][2];//0:무임승차 1:유임승차
+int dijkstra() {
+	int dist[205][2];//0:무임승차 1:유임승차
 	for (int i = 0; i < 205; i++) {
 		dist[i][0] = INF;
 		dist[i][1] = INF;
 	}
 
-	priority_queue<path, vector<path>, compair_path>pq;
+	priority_queue<path, vector<path>, greater<path>>pq;
 
 	dist[start][0] = 0;
-	dist[start][1] = basicprice;
-	path pa(0, 0, basicprice, start);
-	pq.push(pa);
-    int curr;
-	float currdist_0, currdist_1, nextdist_0, nextdist_1;
+	dist[start][1] = basicprice*100;
+	pq.emplace(0, 0, basicprice*100, start);
+	int curr;
+	int currdist_0, currdist_1, nextdist_0, nextdist_1;
 	while (!pq.empty()) {
 		curr = pq.top().dest;
 		currdist_0 = pq.top().dist_0;
 		currdist_1 = pq.top().dist_1;
 		pq.pop();
 
-		if (currdist_0 != dist[curr][0] ||  currdist_1 != dist[curr][1]) continue;
+		if (currdist_0 != dist[curr][0] || currdist_1 != dist[curr][1]) continue;
 
 
 		for (road next : edge[curr]) {
 
 			nextdist_0 = min(currdist_0, currdist_1) + (next.dist*price_1 + basicfine)*next.p;
-			nextdist_1 = min(currdist_0 + basicprice, currdist_1) + next.dist*price_1;
+			nextdist_1 = min(currdist_0 + basicprice*100, currdist_1) + next.dist*price_1*100;
 
-			if ( min(nextdist_0, nextdist_1) < min(dist[next.dest][0], dist[next.dest][1]) ) {
-					dist[next.dest][0] = min(dist[next.dest][0], nextdist_0);
-					dist[next.dest][1] = min(dist[next.dest][1], nextdist_1);
-					pq.push(path(min(dist[next.dest][0], dist[next.dest][1]), dist[next.dest][0], dist[next.dest][1], next.dest));
+			if (min(nextdist_0, nextdist_1) < min(dist[next.dest][0], dist[next.dest][1])) {
+				dist[next.dest][0] = min(dist[next.dest][0], nextdist_0);
+				dist[next.dest][1] = min(dist[next.dest][1], nextdist_1);
+				pq.emplace(min(dist[next.dest][0], dist[next.dest][1]), dist[next.dest][0], dist[next.dest][1], next.dest);
 			}
 		}
 	}
@@ -115,7 +96,7 @@ int main() {
 	while (testcase--)
 	{
 		getinput();
-		printf("%.2f\n", dijkstra());
+		printf("%.2f\n", (float)dijkstra()/100);
 
 		for (int i = 0; i < 205; i++)
 			edge[i].clear();
